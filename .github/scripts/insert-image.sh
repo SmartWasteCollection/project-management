c8eba984d0fc18fa6e57840b6583e513fbfb3e9a
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-05-26 10:00:00 +0200

    ci: insert img in image paths

--- .github/scripts/insert-image.sh
+++ .github/scripts/insert-image.sh
@@ -1,6 +1,6 @@
-IMAGE_PATH="$1"
+IMAGE_PATH="img/$1"
 if [[ ! -f "$IMAGE_PATH.png" ]]; then
-    IMAGE_PATH="$1.pm"
+    IMAGE_PATH="img/$1.pm"
 fi
 CAPTION=$(cat "doc/appendix/captions/$1.md")
 
@@ -10,5 +10,4 @@ echo "
     \includegraphics[width=\textwidth]{$IMAGE_PATH}
     \caption{$CAPTION \hyperlink{back:$1}{Torna indietro}.}
     \label{fig:$1}
-\end{figure}
-" >> doc/appendix/appendix.tex
+\end{figure}" >> doc/appendix/appendix.tex
